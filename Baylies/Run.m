7251b841b7c4dc9430a0c4b9854dd62c648414f8
Amelia <baylieslab@mski1543.mskcc.root.mskcc.org>
2018-06-11 13:12:19 -0400

    want to checkout

--- Baylies/Run.m
+++ Baylies/Run.m
@@ -1,73 +1,70 @@
 %% Add BioFormats 
 addpath('../Utilities/src/MATLAB')
-%folder = '/Users/baylieslab/Documents/Amelia/some czi/'
 %folder = '/Volumes/baylieslab/Current Lab Members/Whitney/Rhabdomyosarcoma plate movies/17-06-28 final pi3k inhibitors/'
 folder = '/Users/baylieslab/Documents/Amelia/rms/rmsMim/18-03-18_Subset';
 folder2 = '/Users/baylieslab/Documents/Amelia/rms/rmsMim/16-06-23_plate-1_partial';
-% folder = pwd();
+%folder = pwd();
 %folder = uigetdir();
-%disp(class(folder)
+
 dlist = dir(fullfile(folder,'*.czi'));
-%disp(dlist(1));
-%for i = 1:size(dlist)
 fs = [];
 f1 = '/Users/baylieslab/Documents/Amelia/rms/rmsMip/rmsMatip/misc/2018-03-18_plateMap.csv';
 
 [conditions,conditDict] = MakeConditDict(f1);
+
 prefix = '/Users/baylieslab/Documents/Amelia/rms/rmsMim/18-03-18_Subset/18-03-18_';
 pref2 = '18-03-18_';
 suffix = '.czi';
-for condition = conditions(11)
+
+for condition = conditions(9)
+    %conditVelocs = cell
     disp(condition{1})
-    %disp(class(condition))
     
     wells = conditDict(condition{1});
+    conditVeloc = cell(3);
     for well = wells 
         wellF = strcat(prefix,well{1},suffix);
         wellN = strcat(pref2,well{1},suffix);
         if exist(wellF, 'file')
             disp(wellN)
-    %end
-        %for d = dlist :
         
         
-    %     disp(conditDict(key{1}))
 
-% end
-% 
-% for i = 4
-
-
-    %% Get Images
-    %Name = fullfile(folder,dlist(i).name);
-    %Name = fullfile(folder,dlist(i).name);
-%         try
+            %try
             temp = figure('Name',condition{1});
             fs = [fs,temp];
+            
+            %% Get Images
             [im,imd] =  MicroscopeData.Original.ReadData(folder,wellN);
             
-            %figure(fs(1,1));
-            %figure(1)
+            
 
             [cells] = segIms(im);
 
-            %figure(fs(1,1));
-            %fs(1,1);
+            
             [cells2,Edges] = getTracks(cells,size(im));
 
 
             %DrawTracks(squeeze(im),cells2,wellF);
 
             %ExportTrackStats(cells2,size(im),wellF)
+            
+            AllSegs = vertcat(cells2{:});
+            AllTracks = [AllSegs.Tid];
+            Tracks = unique(AllTracks);
+            cellCount = size(Tracks,2)
+            wellVeloc = cell(size(im,5),size(Tracks,2));
+            
             ShowPlot(cells2,size(im),condition,well{1},fs)
 
-%         catch e
-%             disp("i = " + i + ", well " + wellN(end-6:end-4))
-%             disp("exception: " + e.identifier)
-%             disp("except.message: " + e.message)
-%             continue
-% 
-%         end
+    %         catch e
+    %             disp("i = " + i + ", well " + wellN(end-6:end-4))
+    %             disp("exception: " + e.identifier)
+    %             disp("except.message: " + e.message)
+    %             continue
+    % 
+    %         end
+
         end
     end
 
